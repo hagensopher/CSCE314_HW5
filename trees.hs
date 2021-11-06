1d7008127c7847dbdf03bb28eb44f06109841b27
hagensopher <hagensopher13@gmail.com>
2021-11-06 14:15:29 -0500

    formatting

--- trees.hs
+++ trees.hs
@@ -16,12 +16,12 @@ leafCollection :: Tree a b  -> [a] --needs to be a common type c
 leafCollection (Leaf a) = [a]
 leafCollection (Branch b l r) = leafCollection l ++ leafCollection r
 
-mapleaves leaves = map show leaves
-
 bcol :: Tree a b  -> [b] --needs to be a common type c
 bcol (Leaf a) = []
 bcol (Branch b l r) = [b] ++ bcol l ++ bcol r
+mapleaves leaves = map show leaves --makes a common type c?
+mapbranches branch = map show branch --makes a common type c?
+
 --preorder takes a function 1 which gets all the leaves to common type c
 -- function two gets all the branches to common type c 
 --preorder :: (a->c) -> (b->c) -> Tree a b -> [c]
-mapbranches branch = map show branch
