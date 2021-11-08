994d65ea8cffd2ad35ba4f20024bd2ef85e1f97e
hagensopher <hagensopher13@gmail.com>
2021-11-07 18:25:19 -0600

    fixed ordering is prod

--- sets.hs
+++ sets.hs
@@ -9,7 +9,13 @@ subset set1 set2 = set1 == [x | x <- set2, x `elem` set1]
 
 setEqual:: Eq a => Set a -> Set a -> Bool 
 setEqual set1 set2 = if subset set1 set2 then length set1 == length set2 else False
-
-setProd:: (Eq t, Eq t1) => Set t -> Set t1 -> Set (t,t1)
-setProd set1 set2 = zip (take l (cycle set1)) (take l (cycle set2))
+--not ordered right :(
+setProd2:: (Eq t, Eq t1) => Set t -> Set t1 -> Set (t,t1)
+setProd2 set1 set2 = zip (take l (cycle set1)) (take l (cycle set2))
                     where l = lcm (length set1) (length set2)
+
+--trying it again where sets is ordered 
+--myfunction from homework 3 drills
+setProd:: (Eq t, Eq t1) => Set t -> Set t1 -> Set (t,t1)
+setProd set1 set2 = zip (concatMap (replicate (length set2)) set1) (take l (cycle set2))
+                        where l = lcm (length set1) (length set2)
